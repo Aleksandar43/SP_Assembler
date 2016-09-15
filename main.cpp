3e218364de36a8dbc49686e2a44473348f42efaf
Aleksandar43 <a.plahcinski@gmail.com>
2016-09-15 18:35:16 +0200

    +TabelaSimbola

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
+#include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
+#include "TabelaSimbola.h"
 
 using namespace std;
 
-int main()
+int main(int argc,char* argv[])
 {
-    cout << "Hello woooooooooooooooooorld!2222" << endl;
+    if(argc<2){
+        cout<<"Nije naveden fajl!\n";
+        return 0;
+    }
+    string imeFajla=argv[1];
+    FILE *fajl=fopen(argv[1],"r");
+    if(fajl==nullptr) {
+        cout<<"Ne postoji fajl sa tim imenom!\n";
+        return 0;
+    }
+    /*Ukloniti komentare?*/
+    char *broj="0x200";
+    int x=atoi(broj);
+    cout<<x<<endl;
+    TabelaSimbola::dodajSimbol("labela1","text",8,'l');
+    TabelaSimbola::ispisi();
+    fclose(fajl);
     return 0;
 }
